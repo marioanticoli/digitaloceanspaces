1ff8e52cd9eba6c9aed21bb41d1eac03c30e90f0
Mario Anticoli <mario@imrepublic.com>
2020-01-23 16:01:28 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const aws = require('aws-sdk');
+
+
+aws.config.getCredentials(function (err) {
+  if (err) console.log(err.stack);
+  else {
+    console.log("Access key:", aws.config.credentials.accessKeyId);
+    console.log("Secret access key:", aws.config.credentials.secretAccessKey);
+  }
+});
+
+const spacesEndpoint = new aws.Endpoint('sfo2.digitaloceanspaces.com');
+const bucket = "aris-sample-imr";
+
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint
+});
+
+
+function getBuckets(prefix) {
+  const params = {
+    Bucket: bucket,
+    Prefix: prefix
+  };
+
+  s3.listObjectsV2(params, (err, data) => {
+    if (err) {
+      console.log("ERROR");
+    }
+    data.Contents.forEach((el) => {
+      if (el.Size != 0) {
+        console.log(el.Key, el.LastModified);
+      }
+    });
+  });
+}
+
+getBuckets('qr-s010/sensor_data/2019-11-01/lucint_imagery/');
